a2f49f24f72db92037a252580b2495294632b045
Yan-Freitas <yf590001@gmail.com>
2025-07-07 13:00:41 -0300

    comitado

--- test.c
+++ test.c
@@ -2,19 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
-
+#include <time.h>
 int main ( void )
 {
+   srand(time(NULL));
    char num[20];
    char med[50]="C:\\Users\\Public\\Documents\\Trabalho\\Medicos\\medico";
-   int i;
+   int id;
+   int idade;
+   int idmed;
+   int estado;
    FILE *fptr;
-   for ( i = 1; i <= 11; ++i )
+   for (int i = 1; i <= 10; ++i )
    {
+      id = rand()%8999+1000;
+      idade = rand()%99+1;
+      idmed = rand()%8999+1;
+      estado = rand()%3+1;
       itoa(i,  num, 10);
       strcat(med,num);
       fptr = fopen(("%s",med),"w");
-      fprintf(fptr,"ID:,NOME:,IDADE:,IDMED:,ESTADO:");
+      fprintf(fptr,"ID:%d,NOME:Jose,IDADE:%d,IDMED:%d,ESTADO:%d",id,idade,idmed,estado);
       fclose(fptr);
       med[49]=0;
    }
